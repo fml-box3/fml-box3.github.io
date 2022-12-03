2f8a1d1b3b0ca3e1c04ecd082b324c1f2c93a20d
fml-box3 <110579473+fml-box3@users.noreply.github.com>
2022-12-03 20:50:59 +0800

    Update president-jiangzemin.js

--- president-jiangzemin/president-jiangzemin.js
+++ president-jiangzemin/president-jiangzemin.js
@@ -5,7 +5,7 @@ function more(){
 
 	var iframe = document.createElement('iframe');
 	div = document.getElementById('content');
-	iframe.src = 'https://baike.baidu.com/'
+	iframe.src = 'https://baike.baidu.com/item/%E6%B1%9F%E6%B3%BD%E6%B0%91/115299'
 	iframe.width = 800;
 	iframe.height = 600;
 	div.appendChild(iframe);
