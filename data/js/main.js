beaf8cf53c1c22aa1e8081296339b3e906e9f4af
fml-box3 <110579473+fml-box3@users.noreply.github.com>
2022-12-05 20:46:16 +0800

    Create main.js

---
+++ data/js/main.js
@@ -0,0 +1,17 @@
+if (new Date().getDate() == 6) {
+    const addhtml = ((position, localName, data, html) => {
+        var newHtml = document.createElement(localName);
+        for (var name in data) {
+            newHtml.setAttribute(name, data[name]);
+        }
+        newHtml.innerHTML = html;
+        var newElement = position.appendChild(newHtml);
+        return (newElement);
+    });
+    addhtml(document.body, "style", { type: "text/css" }, `
+    html {
+    filter: progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);
+    -webkit-filter: grayscale(100%);
+    }
+    `);
+}
