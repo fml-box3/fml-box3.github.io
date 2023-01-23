45c676300b68235f9dbcbc8fae673c2ca7d9a8d4
fml-box3 <110579473+fml-box3@users.noreply.github.com>
2023-01-23 21:11:50 +0800

    Create index.js

--- data/js/index.js
+++ data/js/index.js
@@ -1,4 +1,6 @@
-let name = 'FML饭米粒'
-document.querySelector("#footer-text").innerHTML=`
-	©${new Date().getFullYear()}${name.link('https://github.com/fml-box3')}
-`
+setTimeout(() => {
+	let name = 'FML饭米粒'
+	document.querySelector("#footer-text").innerHTML=`
+		©${new Date().getFullYear()}${name.link('https://github.com/fml-box3')}
+	`
+},500)
