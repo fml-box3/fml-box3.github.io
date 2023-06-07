beed64aaa8e255b231b2349cb35a595db35bf398
fml-box3 <110579473+fml-box3@users.noreply.github.com>
2023-06-07 20:07:52 +0800

    Update Runner.js

--- JS-Runner/Runner.js
+++ JS-Runner/Runner.js
@@ -1,20 +1,18 @@
-function sleep(ms) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(() => {
-            console.log('sleep end');
-            resolve();
-        }, ms);
-    });
-}
-
 (async function () => {
+	 function sleep(ms) {
+	    return new Promise(function (resolve, reject) {
+		setTimeout(() => {
+		    console.log('sleep end');
+		    resolve();
+		}, ms);
+	    });
+	}
  	while(true){
 		await sleep(500)
 		var code = document.getElementById('code')
 		var out = document.getElementById('output')
 		if (code && out) break
 	}
-
 	function clean(){
 		out.value = ''
 	}
