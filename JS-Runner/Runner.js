c70748fece6842aeb34f8e85b5e1ce2e832495a3
fml-box3 <110579473+fml-box3@users.noreply.github.com>
2023-06-07 20:05:54 +0800

    Update Runner.js

--- JS-Runner/Runner.js
+++ JS-Runner/Runner.js
@@ -1,22 +1,37 @@
-var code = document.getElementById('code')
-var out = document.getElementById('output')
+function sleep(ms) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => {
+            console.log('sleep end');
+            resolve();
+        }, ms);
+    });
+}
 
-function clean(){
-	out.value = ''
-}
-function show(string){
-	var value = out.value
-   if (value)
-	   value += '\n' + string
-	else
-		value = string
-	out.value = value
-}
-function run(){
-	clean()
-	try {
-		eval(code)
-	} catch(error) {
-		show(error)
+(async function () => {
+ 	while(true){
+		await sleep(500)
+		var code = document.getElementById('code')
+		var out = document.getElementById('output')
+		if (code && out) break
 	}
-}
+
+	function clean(){
+		out.value = ''
+	}
+	function show(string){
+		var value = out.value
+	   if (value)
+		   value += '\n' + string
+		else
+			value = string
+		out.value = value
+	}
+	function run(){
+		clean()
+		try {
+			eval(code)
+		} catch(error) {
+			show(error)
+		}
+	}
+})()
