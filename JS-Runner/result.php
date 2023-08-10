c295be5afcf56b0c1130eb43fbcfdc2d161625db
fml-box3 <110579473+fml-box3@users.noreply.github.com>
2023-08-10 09:03:45 +0800

    Create result.php

---
+++ JS-Runner/result.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <?php
+echo $_POST['HTML'];
+        ?>
+        <textarea id="out'>console:</textarea>
+        <script src="/JS-Runner/Runner.js"></script>
+    </body>
+</html>
