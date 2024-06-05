c1305867a8b12cfee39de54506b5955a50f756e0
fml-box3 <110579473+fml-box3@users.noreply.github.com>
2024-06-05 20:18:29 +0800

    Update mainJS.js

--- data/js/mainJS.js
+++ data/js/mainJS.js
@@ -1,20 +1,3 @@
-if (false) {
-    const addhtml = ((position, localName, data, html) => {
-        var newHtml = document.createElement(localName);
-        for (var name in data) {
-            newHtml.setAttribute(name, data[name]);
-        }
-        newHtml.innerHTML = html;
-        var newElement = position.appendChild(newHtml);
-        return (newElement);
-    });
-    addhtml(document.body, "style", { type: "text/css" }, `
-    html {
-    filter: progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);
-    -webkit-filter: grayscale(100%);
-    }
-    `);
-}
 document.oncontextmenu = () => false;
 document.onselectstart = () => false;
 document.onkeyup = () => false;
